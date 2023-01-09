9be230b6bebeb2054c40b02e82e8e279070f569c
Rabah Boulhares <rabah.boulharespro@outlook.fr>
2023-01-09 11:31:13 +0100

    Update script.js

--- AICHAT/client/script.js
+++ AICHAT/client/script.js
@@ -75,7 +75,7 @@ const handleSubmit = async (e) => {
   const messageDiv = document.getElementById(uniqueId);
   loader(messageDiv);
 
-  const response = await fetch("http://localhost:5001", {
+  const response = await fetch("httpps://rabah-chatgpt-openai.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
